f4fd12a67eed44f508bdbb160aa4fff0ee5d0473
GoingJack <1312159064@qq.com>
2025-03-17 23:54:10 +0800

    feat: setWindowTitle

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -9,6 +9,8 @@ MainWindow::MainWindow(QWidget* parent)
     : QMainWindow(parent), ui(new Ui::MainWindow) {
   ui->setupUi(this);
 
+  setWindowTitle(tr("QtElu"));
+
   lcu_service_thread_.setObjectName("NetWorkThread");
   lcu_service_thread_.start();
   GetWillumpService().moveToThread(&lcu_service_thread_);
