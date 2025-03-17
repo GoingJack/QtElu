9d62b860836b4dd01744e18a20602dea2fcff299
GoingJack <goingjack@foxmail.com>
2025-03-17 13:53:13 +0800

    feat: add git action yml file

---
+++ scripts/windows-publish.ps1
@@ -0,0 +1,35 @@
+[CmdletBinding()]
+param (
+    [string] $archiveName, [string] $targetName
+)
+
+$scriptDir = $PSScriptRoot
+$currentDir = Get-Location
+Write-Host "currentDir" $currentDir
+Write-Host "scriptDir" $scriptDir
+
+function Main() {
+
+    New-Item -ItemType Directory $archiveName
+    # copy exe
+    Copy-Item output\$targetName $archiveName\
+    # copy dependencies 
+    windeployqt --qmldir . --plugindir $archiveName\plugins --no-translations --compiler-runtime $archiveName\$targetName
+    # delete file
+    $excludeList = @("*.qmlc", "*.ilk", "*.exp", "*.lib", "*.pdb")
+    Remove-Item -Path $archiveName -Include $excludeList -Recurse -Force
+    # copy vcRedist dll
+    $redistDll="{0}{1}\*.CRT\*.dll" -f $env:vcToolsRedistDir.Trim(),$env:msvcArch
+    Copy-Item $redistDll $archiveName\
+    # copy WinSDK dll
+    $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir.Trim(),$env:winSdkVer.Trim(),$env:msvcArch
+    Copy-Item $sdkDll $archiveName\
+    # zip
+    Compress-Archive -Path $archiveName $archiveName'.zip'
+}
+
+if ($null -eq $archiveName || $null -eq $targetName) {
+    Write-Host "args missing, archiveName is" $archiveName ", targetName is" $targetName
+    return
+}
+Main
